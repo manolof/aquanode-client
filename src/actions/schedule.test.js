390503acf6edcf977790d48cdef53d9fc65eee60
Emmanouil <manolis09@gmail.com>
2018-08-21 18:22:44 +0200

    test(schedule): add more tests related to schedule actions

--- src/actions/schedule.test.js
+++ src/actions/schedule.test.js
@@ -1,15 +1,26 @@
-import { getSchedule } from '../services/schedule';
+import { getSchedule, resetSchedule, setSchedule } from '../services/schedule';
 import {
 	getScheduleAction,
 	getScheduleFailAction,
 	getScheduleStartAction,
 	getScheduleSuccessAction,
+	resetScheduleAction,
+	resetScheduleFailAction,
+	resetScheduleStartAction,
+	resetScheduleSuccessAction,
+	setScheduleAction,
+	setScheduleFailAction,
+	setScheduleStartAction,
+	setScheduleSuccessAction,
 	ScheduleActions,
 } from './schedule';
 
 jest.mock('../services/schedule');
 
 describe('schedule actions', () => {
+	const mockSuccessResponse = { data: { res: 'test' } };
+	const mockErrorResponse = { error: 'Something went wrong' };
+
 	afterEach(() => {
 		jest.resetAllMocks();
 	});
@@ -42,13 +53,59 @@ describe('schedule actions', () => {
 		});
 	});
 
+	it('should create RESET Start action', () => {
+		expect(resetScheduleStartAction()).toEqual({
+			type: ScheduleActions.RESET_START,
+		});
+	});
+
+	it('should create RESET Success action', () => {
+		const payload = {};
+
+		expect(resetScheduleSuccessAction(payload)).toEqual({
+			type: ScheduleActions.RESET_SUCCESS,
+			payload,
+		});
+	});
+
+	it('should create RESET Fail action', () => {
+		const error = {};
+
+		expect(resetScheduleFailAction(error)).toEqual({
+			type: ScheduleActions.RESET_FAIL,
+			error,
+		});
+	});
+
+	it('should create SET Start action', () => {
+		expect(setScheduleStartAction()).toEqual({
+			type: ScheduleActions.SET_START,
+		});
+	});
+
+	it('should create SET Success action', () => {
+		const payload = {};
+
+		expect(setScheduleSuccessAction(payload)).toEqual({
+			type: ScheduleActions.SET_SUCCESS,
+			payload,
+		});
+	});
+
+	it('should create SET Fail action', () => {
+		const error = {};
+
+		expect(setScheduleFailAction(error)).toEqual({
+			type: ScheduleActions.SET_FAIL,
+			error,
+		});
+	});
+
 	describe('getScheduleAction', () => {
 		let dispatch;
-		const mockResponse = { data: { res: 'test' } };
 
 		beforeAll(() => {
 			dispatch = jest.fn();
-			getSchedule.mockImplementation(() => Promise.resolve(mockResponse));
 		});
 
 		afterEach(() => {
@@ -59,12 +116,90 @@ describe('schedule actions', () => {
 			jest.restoreAllMocks();
 		});
 
-		it('should fetch the schedule', async () => {
+		it('should dispatch a success action, if it successfully calls the service', async () => {
+			getSchedule.mockImplementation(() => Promise.resolve(mockSuccessResponse));
 			await getScheduleAction()(dispatch);
 
 			expect(dispatch).toHaveBeenCalledTimes(2);
 			expect(dispatch).toHaveBeenCalledWith(getScheduleStartAction());
-			expect(dispatch).toHaveBeenCalledWith(getScheduleSuccessAction(mockResponse));
+			expect(dispatch).toHaveBeenCalledWith(getScheduleSuccessAction(mockSuccessResponse));
+		});
+
+		it('should dispatch a fail action, if it fails to call the service', async () => {
+			getSchedule.mockImplementation(() => Promise.reject(mockErrorResponse));
+			await getScheduleAction()(dispatch);
+
+			expect(dispatch).toHaveBeenCalledTimes(2);
+			expect(dispatch).toHaveBeenCalledWith(getScheduleStartAction());
+			expect(dispatch).toHaveBeenCalledWith(getScheduleFailAction(mockErrorResponse));
+		});
+	});
+
+	describe('resetScheduleAction', () => {
+		let dispatch;
+
+		beforeAll(() => {
+			dispatch = jest.fn();
+		});
+
+		afterEach(() => {
+			jest.resetAllMocks();
+		});
+
+		afterAll(() => {
+			jest.restoreAllMocks();
+		});
+
+		it('should dispatch a success action, if it successfully calls the service', async () => {
+			resetSchedule.mockImplementation(() => Promise.resolve(mockSuccessResponse));
+			await resetScheduleAction()(dispatch);
+
+			expect(dispatch).toHaveBeenCalledTimes(2);
+			expect(dispatch).toHaveBeenCalledWith(resetScheduleStartAction());
+			expect(dispatch).toHaveBeenCalledWith(resetScheduleSuccessAction(mockSuccessResponse));
+		});
+
+		it('should dispatch a fail action, if it fails to call the service', async () => {
+			resetSchedule.mockImplementation(() => Promise.reject(mockErrorResponse));
+			await resetScheduleAction()(dispatch);
+
+			expect(dispatch).toHaveBeenCalledTimes(2);
+			expect(dispatch).toHaveBeenCalledWith(resetScheduleStartAction());
+			expect(dispatch).toHaveBeenCalledWith(resetScheduleFailAction(mockErrorResponse));
+		});
+	});
+
+	describe('setScheduleAction', () => {
+		let dispatch;
+
+		beforeAll(() => {
+			dispatch = jest.fn();
+		});
+
+		afterEach(() => {
+			jest.resetAllMocks();
+		});
+
+		afterAll(() => {
+			jest.restoreAllMocks();
+		});
+
+		it('should dispatch a success action, if it successfully calls the service', async () => {
+			setSchedule.mockImplementation(() => Promise.resolve(mockSuccessResponse));
+			await setScheduleAction()(dispatch);
+
+			expect(dispatch).toHaveBeenCalledTimes(2);
+			expect(dispatch).toHaveBeenCalledWith(setScheduleStartAction());
+			expect(dispatch).toHaveBeenCalledWith(setScheduleSuccessAction(mockSuccessResponse));
+		});
+
+		it('should dispatch a fail action, if it fails to call the service', async () => {
+			setSchedule.mockImplementation(() => Promise.reject(mockErrorResponse));
+			await setScheduleAction()(dispatch);
+
+			expect(dispatch).toHaveBeenCalledTimes(2);
+			expect(dispatch).toHaveBeenCalledWith(setScheduleStartAction());
+			expect(dispatch).toHaveBeenCalledWith(setScheduleFailAction(mockErrorResponse));
 		});
 	});
 });
