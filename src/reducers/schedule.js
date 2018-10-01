8f066e362858620f5d094011f70983c827ed59eb
Emmanouil <3025620+manolof@users.noreply.github.com>
2018-10-01 15:10:40 +0200

    refactor(schedule): set and pass override property from the store

--- src/reducers/schedule.js
+++ src/reducers/schedule.js
@@ -1,13 +1,26 @@
 import { ScheduleActions } from '../actions/schedule';
 
-const initialState = [];
+export const initialState = {
+	override: false,
+	items: [],
+};
 
 const scheduleReducer = (state = initialState, action) => {
+	const items = action.payload;
+
 	switch (action.type) {
 		case ScheduleActions.GET_SUCCESS:
 		case ScheduleActions.RESET_SUCCESS:
+			return {
+				...initialState,
+				items,
+			};
+
 		case ScheduleActions.SET_SUCCESS:
-			return action.payload;
+			return {
+				...initialState,
+				override: true,
+			};
 
 		default:
 			return state;
--- src/reducers/status.test.js
+++ src/reducers/status.test.js
@@ -1,25 +1,22 @@
-import reducer from './status';
+import reducer, { initialState } from './status';
 import { StatusActions } from '../actions/status';
+
+const mockStatusPayload = {
+	time: '2018-10-23',
+	entities: [
+		{
+			type: 'lights',
+			status: 'night',
+		},
+	],
+};
 
 describe('Status reducer', () => {
 	it('should return the initial state', () => {
-		expect(reducer(undefined, {})).toEqual({
-			time: '',
-			entities: [],
-		});
+		expect(reducer(undefined, {})).toEqual(initialState);
 	});
 
 	it('should handle GET_SUCCESS', () => {
-		const mockStatusPayload = {
-			time: '2018-10-23',
-			entities: [
-				{
-					type: 'lights',
-					status: 'night',
-				},
-			],
-		};
-
 		expect(
 			reducer({}, {
 				type: StatusActions.GET_SUCCESS,
